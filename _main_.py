42c37cc7c0ff194f31a7287c62187fe8a011849f
sujitvinu123 <sujitvinurr@gmail.com>
2025-03-22 17:24:08 +0530

    Create _main_.py

---
+++ _main_.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+"""
+Sherlock: Find Usernames Across Social Networks Module
+
+This module contains the main logic to search for usernames at social
+networks.
+"""
+
+def main():
+    print("Running the Sherlock application...")
+
+if __name__ == "__main__":
+    main()
